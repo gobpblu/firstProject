673d47311310710855332793931f286ca136509c
Max Abramov <gobpo2002@gmail.com>
2024-03-16 13:50:56 +0600

    new updates

--- src/classesAndObjects/CockExample1.java
+++ src/classesAndObjects/CockExample1.java
@@ -3,9 +3,9 @@ package classesAndObjects;
 public class CockExample1 {
     public static void main(String[] args) {
         Cock cock1 = new Cock();
+        cock1.weight = 4.3;
         cock1.age = 5;
         cock1.isAlive = false;
-        cock1.weight = 3.4;
         System.out.println("ПЕТУХ 1: " +
                 "возраст: " + cock1.age +
                 ", живой: " + cock1.isAlive +
@@ -19,14 +19,5 @@ public class CockExample1 {
                 "возраст: " + cock2.age +
                 ", живой: " + cock2.isAlive +
                 ", вес: " + cock2.weight);
-
-        Cock cock3 = new Cock();
-        cock3.age = 3;
-        cock3.isAlive = true;
-        cock3.weight = 2;
-        System.out.println("ПЕТУХ 3: " +
-                "возраст: " + cock3.age +
-                ", живой: " + cock3.isAlive +
-                ", вес: " + cock3.weight);
     }
 }
